46d0e104d809e8d50d557906580956912c38c435
Student <jonathan1113478@gmail.com>
2023-03-19 11:38:10 +0200

    fix firebase

--- app/src/main/java/com/example/helpap/RegisterActivity.java
+++ app/src/main/java/com/example/helpap/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.helpap;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,7 +10,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,6 +23,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     Button buttonLog3;
     TextView loginTxt2;
+    private FirebaseAuth mAuth;
+
 
 
 
@@ -25,6 +32,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
 
         buttonLog3 = findViewById(R.id.buttonLog3);
         buttonLog3.setOnClickListener(this);
@@ -56,6 +65,29 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
 
     }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+
+    }
+
 
 
 }
--- app/src/main/java/com/example/helpap/welcomeActivity.java
+++ app/src/main/java/com/example/helpap/welcomeActivity.java
@@ -72,26 +72,4 @@ public class welcomeActivity extends AppCompatActivity implements View.OnClickLi
             startActivity(intent);
         }
     }
-
-    private void createAccount(String email, String password) {
-        // [START create_user_with_email]
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(welcomeActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-        // [END create_user_with_email]
-
-    }
 }
